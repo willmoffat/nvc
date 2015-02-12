46ac533d0c9381af02de9f87cccfa923eebd5653
Will Moffat <will@hamstersoup.com>
2015-02-12 10:19:22 +0100

    refactor.

--- window.js
+++ window.js
@@ -1,4 +1,5 @@
 // UI code.
+"use strict";
 
 var textarea = document.querySelector('textarea');
 var errlog = document.querySelector('#errlog');
@@ -11,6 +12,10 @@ function showError(err) {
   console.error(err, err.stack);
   errlog.textContent += err + '\n';
 }
+
+///////////
+// Dirty //
+///////////
 
 var dirtMonitor = (function() {
   var isDirty = false;
@@ -34,116 +39,168 @@ var dirtMonitor = (function() {
   };
 })();
 
-// TODO(wdm) Check that SEPERATOR does not appear in notes.
-var SEPERATOR = '\n[//]: # (NVC: Do not edit this line)\n\n';
 
-var notes = [];
-
-var selected = null;
-
-function setSelected(i) {
-  if (typeof selected === "number") {
-    notes[selected] = parseNote(textarea.value);
-    var listItem = document.querySelector('#noteList ul').children[selected];
-    fillListItem(listItem, notes[selected]);
-    listItem.classList.remove('selected');
-  }
-  selected = i;
-  document.querySelector('#noteList ul')
-      .children[selected]
-      .classList.add('selected');
-  updateTextArea(notes[i].text);
-}
 
 ////////////
 // Search //
 ////////////
 
-var index;
-var listItems;
+var search = (function() {
 
-function createIndex() {
-  index = lunr(function() {
-    this.field('title', {boost: 10});
-    this.field('text');
-    this.ref('id');
-  });
-  notes.forEach(function(n) { index.add(n); });
-}
+  var index;
+  var listItems;
+  var selected = null;
 
-function showAll() {
-  listItems.forEach(function(item) { item.style.display = ''; });
-}
-
-function hideAll() {
-  listItems.forEach(function(item) { item.style.display = 'none'; });
-}
-
-function onSearch(e) {
-  var q = e.target.value;
-  if (!q.length) {
-    showAll();
-    return;
+  function createIndex() {
+    index = lunr(function() {
+      this.field('title', {boost: 10});
+      this.field('text');
+      this.ref('id');
+    });
+    DB.getNotes().forEach(function(n) { index.add(n); });
   }
 
-  var results = index.search(q);
-  hideAll();
-  results.forEach(function(r) {
-    var i = parseInt(r.ref, 10);
-    listItems[i].style.display = '';
-  });
-  // TOOD(wdm) Something with score?
-}
+  function setSelected(opt_index) {
+    // Calling setSelected without an index just persists any textarea changes.
+    var i = (typeof opt_index === 'number') ? opt_index : selected;
 
-function parseDB(rawText) {
-  var rawNotes = rawText.split(SEPERATOR);
-  notes = rawNotes.map(parseNote);
-  updateNoteList();
-  setSelected(0);
-  createIndex();
-}
+    if (typeof selected === "number") {
+      // Store any edits before switching note.
+      DB.getNotes()[selected] = DB.parseNote(textarea.value);  // HACK
+      var listItem = document.querySelector('#noteList ul').children[selected];
+      fillListItem(listItem, DB.getNotes()[selected]);
+      listItem.classList.remove('selected');
+    }
+    selected = i;
+    document.querySelector('#noteList ul')
+        .children[selected]
+        .classList.add('selected');
+    // Set textarea to current note.
+    updateTextArea(DB.getNotes()[i].text);
+  }
 
-function parseNote(rawNote, i) {
-  var firstLine = rawNote.indexOf('\n');
+  function showAll() {
+    listItems.forEach(function(item) { item.style.display = ''; });
+  }
+
+  function hideAll() {
+    listItems.forEach(function(item) { item.style.display = 'none'; });
+  }
+
+  function onInput(e) {
+    var q = e.target.value;
+    if (!q.length) {
+      showAll();
+      return;
+    }
+
+    var results = index.search(q);
+    hideAll();
+    results.forEach(function(r) {
+      var i = parseInt(r.ref, 10);
+      listItems[i].style.display = '';
+    });
+    // TOOD(wdm) Something with score?
+  }
+
+  function fillListItem(li, note) {
+    li.children[0].textContent = note.title;
+    li.children[1].textContent = note.summary;
+  }
+
+  function updateNoteList() {
+    listItems = [];
+    var ul = document.createElement('ul');
+    DB.getNotes().forEach(function(note) {
+      var li = document.createElement('li');
+      li.innerHTML = '<span></span><i></i>';
+      fillListItem(li, note);
+      ul.appendChild(li);
+      listItems.push(li);
+    });
+    document.querySelector('#noteList').appendChild(ul);
+    setSelected(0);  // HACK?
+  }
+
+  function onClickList(e) {
+    var target = e.target;
+    // This is a nasty hack to move clicks on the <i> tags up to their <li>.
+    if (target.parentNode.nodeName == 'LI') {
+      target = target.parentNode;
+    }
+    if (target.nodeName !== 'LI') {
+      return;
+    }
+    var list = toArray(document.querySelector('#noteList ul').children);
+    var i = list.indexOf(target);
+    setSelected(i);
+  }
+
+  function init() {
+    updateNoteList();
+    createIndex();
+  }
+
   return {
-    id: i,
-    title: rawNote.substring(0, firstLine),
-    summary: rawNote.substring(firstLine + 1, 50),
-    text: rawNote
+    onInput: onInput,
+    onClickList: onClickList,
+    setSelected: setSelected,
+    init: init
   };
-}
+})();
 
 
-// TODO(wdm) Better name!
-function notesAsBlob() {
-  if (typeof selected === "number") {
-    notes[selected] = parseNote(textarea.value);
+////////
+// DB //
+////////
+
+var DB = (function() {
+
+  // TODO(wdm) Check that SEPERATOR does not appear in notes.
+  var SEPERATOR = '\n[//]: # (NVC: Do not edit this line)\n\n';
+
+  // In memory note objects.
+  var notes = [];
+
+  function parseDB(rawText) {
+    var rawNotes = rawText.split(SEPERATOR);
+    notes = rawNotes.map(parseNote);
+    search.init();
   }
-  var texts = notes.map(function(note) { return note.text; });
-  var rawText = texts.join(SEPERATOR);
-  return new Blob([rawText], {
-    type:
-      'text/plain'
-  });
-}
 
-function fillListItem(li, note) {
-  li.children[0].textContent = note.title;
-  li.children[1].textContent = note.summary;
-}
+  function parseNote(rawNote, i) {
+    var firstLine = rawNote.indexOf('\n');
+    return {
+      id: i,
+      title: rawNote.substring(0, firstLine),
+      summary: rawNote.substring(firstLine + 1, 50),
+      text: rawNote
+    };
+  }
 
-function updateNoteList() {
-  listItems = [];
-  var ul = document.createElement('ul');
-  notes.forEach(function(note) {
-    var li = document.createElement('li');
-    li.innerHTML = '<span></span><i></i>';
-    fillListItem(li, note);
-    ul.appendChild(li);
-    listItems.push(li);
-  });
-  document.querySelector('#noteList').appendChild(ul);
-}
+  // TODO(wdm) Better name!
+  function notesAsBlob() {
+    search.setSelected();  // Grabs any changes in textarea.
+    var texts = notes.map(function(note) { return note.text; });
+    var rawText = texts.join(SEPERATOR);
+    return new Blob([rawText], {
+      type:
+        'text/plain'
+    });
+  }
+  return {
+    parseDB: parseDB,
+    parseNote: parseNote,
+    notesAsBlob: notesAsBlob,
+    getNotes: function() { return notes; }
+  };
+})();
+
+
+//////////////
+// TextArea //
+//////////////
+
 
 function updateTextArea(text) {
   textarea.value = text;
@@ -177,19 +234,19 @@ function showFilename(fileEntry_) {
   return fileEntry;
 }
 
-function save() {
+function doSave() {
   if (!dirtMonitor.isDirty) {
     console.log('save skipped: not dirty');
     return Promise.resolve();
   }
-  var blob = notesAsBlob();
+  var blob = DB.notesAsBlob();
   return writeFileEntry(fileEntry, blob)
       .then(dirtMonitor.setClean)
       .catch(showError);
 }
 
 function doClose() {
-  save().then(window.close);
+  doSave().then(window.close);
 }
 
 //////////////////
@@ -200,58 +257,45 @@ var CODE_W = 'W'.charCodeAt(0);
 var CODE_S = 'S'.charCodeAt(0);
 var CODE_1 = '1'.charCodeAt(0);
 var CODE_ESC = 27;
+var CODE_ENTER = 13;
 
 function onKeydown(e) {
-  if (e.keyCode === CODE_ESC) {
+  // Dismiss (with save): ESCAPE, Alt-1, Ctrl-W
+  if ((e.keyCode === CODE_ESC) || (e.altKey && e.keyCode === CODE_1) ||
+      (e.ctrlKey && e.keyCode === CODE_W)) {
     e.preventDefault();
     doClose();
-  }
-  if (e.altKey) {
-    if (e.keyCode === CODE_1) {
-      e.preventDefault();
-      doClose();
-    }
-  }
-  if (e.ctrlKey) {
-    if (e.keyCode === CODE_W) {
-      e.preventDefault();
-      doClose();
-    } else if (e.keyCode === CODE_S) {
-      e.preventDefault();
-      save();
-    }
-  }
-}
-
-
-
-function onClickList(e) {
-  console.log(e);
-  var target = e.target;
-  // This is a nasty hack to move clicks on the <i> tags up to their <li>.
-  if (target.parentNode.nodeName == 'LI') {
-    target = target.parentNode;
-  }
-  if (target.nodeName !== 'LI') {
     return;
   }
-  var list = toArray(document.querySelector('#noteList ul').children);
-  var i = list.indexOf(target);
-  setSelected(i);
+
+  // Save: Ctrl-S
+  if (e.ctrlKey && e.keyCode === CODE_S) {
+    e.preventDefault();
+    doSave();
+    return;
+  }
+
+  // New note (press enter in the search box)
+  if (e.keyCode == CODE_ENTER && e.target.id === "search") {
+    // TODO(wdm) doNewNote();
+  }
 }
+
+
 
 function init() {
   restoreChosenFile()
       .then(showFilename)
       .then(loadFileEntry)
-      .then(parseDB)
+      .then(DB.parseDB)
       .catch(showError);
 
   window.addEventListener('keydown', onKeydown);
   textarea.addEventListener('input', dirtMonitor.setDirty);
-  document.querySelector('#noteList').addEventListener('click', onClickList);
   document.querySelector('#chooseFile').addEventListener('click', onChooseFile);
-  document.querySelector('#search').addEventListener('input', onSearch);
+  document.querySelector('#noteList')
+      .addEventListener('click', search.onClickList);
+  document.querySelector('#search').addEventListener('input', search.onInput);
 }
 
 init();
