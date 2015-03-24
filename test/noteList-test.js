69e9791a7e33dc8ba02468680652d5cdf477b87d
Will Moffat <will@hamstersoup.com>
2015-03-24 20:42:56 +0100

    selection

--- test/noteList-test.js
+++ test/noteList-test.js
@@ -13,21 +13,30 @@ tc = {
       text: 'Title test\nBody test\n'
     };
     var terms = ["test"];
-    assert.equals(noteList.test_noteHtml(terms, note),
-                  '<li><b>Title <q>test</q></b><i>Body <q>test</q>\n</i></li>');
+    var expected = '<li><b>Title <q>test</q></b><i>Body <q>test</q>\n</i></li>';
+    assert.equals(noteList.test_noteHtml(terms, note), expected);
   },
 };
-buster.testCase("single note", tc);
+buster.testCase("single note with highlighting", tc);
 
 tc = {
-  "fills in id2el": function() {  // TODO(wdm) stop testing internals.
-    var notes =
-        [{id: 99, title: 'Title test', text: 'Title test\nBody test\n'}];
-    var terms = ["test"];
-    noteList.init(document.createElement('ul'));
+  "selection": function() {  // TODO(wdm) stop testing internals.
+    var ul = document.createElement('ul');
+    noteList.init(ul);
+
+    var notes = [
+      {id: 33, title: 'Title', text: 'Title\nFoo\n'},
+      {id: 99, title: 'Title test', text: 'Title test\nBody test\n'},
+    ];
+    var terms = [];
     noteList.display(terms, notes);
-    assert.equals(noteList.test_id2el(99).innerHTML,
-                  '<b>Title <q>test</q></b><i>Body <q>test</q>\n</i>');
+    var expected = '<li><b>Title</b><i>Foo\n</i></li>\n' +
+                   '<li><b>Title test</b><i>Body test\n</i></li>';
+    assert.equals(ul.innerHTML, expected);
+
+    noteList.test_select(1);
+    assert.match(ul.children, [{}, {className: 'selected'}])
+
   },
 };
-buster.testCase("noteList.display", tc);
+buster.testCase("noteList", tc);
