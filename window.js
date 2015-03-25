651972a1e0840f3433dba0ca4db0de9b1448d933
Will Moffat <will@hamstersoup.com>
2015-03-25 10:41:57 +0100

    Up/down key for selection.

--- window.js
+++ window.js
@@ -382,40 +382,6 @@ var win = (function() {
   };
 })();
 
-//////////////////
-// Key Handling //
-//////////////////
-
-function handleKeys(handlers) {
-  var keyNames = {
-    8: 'Backspace',
-    13: 'Enter',
-    27: 'Escape',
-    38: 'Up',
-    40: 'Down',
-  };
-  return function keyHandler(e) {
-    var k = e.keyCode;
-    var key;
-    if (k > 48) {
-      key = String.fromCharCode(k);
-    } else {
-      key = keyNames[k];
-    }
-    if (!key) {
-      return;
-    }
-    var pressed = (e.ctrlKey ? 'Ctrl-' : '') + (e.altKey ? 'Alt-' : '') + key;
-    var handler = handlers.globalKeys[pressed];
-    if (!handler && (e.target.id !== 'editor')) {
-      handler = handlers.globalExceptInEditor[pressed];
-    }
-    if (handler) {
-      e.preventDefault();
-      handler();
-    }
-  };
-}
 
 
 ////////////////
@@ -548,22 +514,6 @@ function init() {
       .then(search.init)
       .catch(showError);
 
-  var keyHandlers = {
-    globalKeys: {
-      'Escape': search.clearSearch,
-      'Alt-1': win.hide,
-      'Ctrl-W': win.close,
-      'Ctrl-S': backup.save,
-    },
-    globalExceptInEditor: {
-      'Enter': search.selectOrCreateNote,
-      'Down': search.moveSelectionDown,
-      'Up': search.moveSelectionUp,
-      'Ctrl-Backspace': search.deleteSelectedNote,
-    }
-  };
-
-  window.addEventListener('keydown', handleKeys(keyHandlers));
 
 
   // s('#chooseFile').addEventListener('click', backup.onChooseFile);
