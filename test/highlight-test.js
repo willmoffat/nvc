03126c881fa5d00d3bdacccf88741d4c19c5d24e
Will Moffat <will@hamstersoup.com>
2015-03-28 14:02:03 +0100

    Don't create search regexp if no terms.

--- test/highlight-test.js
+++ test/highlight-test.js
@@ -5,6 +5,22 @@ var highlight = require("../lib/highlight");
 var tc;
 
 tc = {
+  "emtpy terms array should give null": function() {
+    var terms = [];
+    assert.equals(highlight.regexpForSearch(terms), null);
+  },
+  "empty term is ignored": function() {
+    var terms = ['foobar', ''];
+    assert.equals(highlight.regexpForSearch(terms), /foobar/i);
+  },
+  "if all terms are excluded then null returned": function() {
+    var terms = ['12', 'ba', ''];
+    assert.equals(highlight.regexpForSearch(terms), null);
+  },
+};
+buster.testCase("regexp", tc);
+
+tc = {
   "regexp arg required": function() {
     assert.exception(function() { highlight.text(); });
   },
